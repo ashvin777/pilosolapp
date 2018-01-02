069af4284398a3da267920dc9541f3598a8d9a8d
Ashvin Suthar <Ashvin.Suthar@Foghorn.io>
2018-01-02 12:34:38 +0530

    first commit

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ const url = "/ui";
 // url for the editor page
 const urledit = "/admin";
 // tcp port to use
-//const listenPort = "18880"; // fix it just because
-const listenPort = parseInt(Math.random()*16383+49152) // or random ephemeral port
+const listenPort = "1880"; // fix it just because
+// const listenPort = parseInt(Math.random()*16383+49152) // or random ephemeral port
 
 const os = require('os');
 const electron = require('electron');
@@ -57,7 +57,15 @@ var settings = {
     httpNodeRoot: "/",
     userDir: userdir,
     flowFile: flowfile,
-    functionGlobalContext: { }    // enables global context
+    functionGlobalContext: {},    // enables global context
+    adminAuth: {
+       type: "credentials",
+       users: [{
+           username: "admin",
+           password: "$2a$08$zZWtXTja0fB1pzD4sHCMyOCMYz2Z6dNbM6tl8sJogENOMcxWV9DN.",
+           permissions: "*"
+       }]
+    }
 };
 
 // Initialise the runtime with a server and settings
@@ -77,55 +85,55 @@ var template = [{
         { type: "separator" },
         { role: 'quit' }
     ]}, {
-    label: 'Node-RED',
+    label: 'Pilosol App',
     submenu: [
         { label: 'Dashboard',
         accelerator: "Shift+CmdOrCtrl+D",
         click() { mainWindow.loadURL("http://localhost:"+listenPort+url); }
         },
-        { label: 'Editor',
-        accelerator: "Shift+CmdOrCtrl+E",
-        click() { mainWindow.loadURL("http://localhost:"+listenPort+urledit); }
-        },
-        { type: 'separator' },
-        { label: 'Documentation',
-        click() { require('electron').shell.openExternal('http://nodered.org/docs') }
-        },
-        { label: 'Flows and Nodes',
-        click() { require('electron').shell.openExternal('http://flows.nodered.org') }
-        },
-        { label: 'Google group',
-        click() { require('electron').shell.openExternal('https://groups.google.com/forum/#!forum/node-red') }
-        }
-    ]}, {
-    label: "Edit",
-    submenu: [
-        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
-        { type: "separator" },
-        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
-    ]}, {
-    label: 'View',
-    submenu: [
-        { label: 'Reload',
-            accelerator: 'CmdOrCtrl+R',
-            click(item, focusedWindow) { if (focusedWindow) focusedWindow.reload(); }
-        },
-        { label: 'Toggle Developer Tools',
-            accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
-            click(item, focusedWindow) { if (focusedWindow) focusedWindow.webContents.toggleDevTools(); }
-        },
-        { type: 'separator' },
-        { role: 'resetzoom' },
-        { role: 'zoomin' },
-        { role: 'zoomout' },
-        { type: 'separator' },
-        { role: 'togglefullscreen' },
-        { role: 'minimize' }
-    ]}
+        // { label: 'Editor',
+        // accelerator: "Shift+CmdOrCtrl+E",
+        // click() { mainWindow.loadURL("http://localhost:"+listenPort+urledit); }
+        // },
+        // { type: 'separator' },
+        // { label: 'Documentation',
+        // click() { require('electron').shell.openExternal('http://nodered.org/docs') }
+        // },
+        // { label: 'Flows and Nodes',
+        // click() { require('electron').shell.openExternal('http://flows.nodered.org') }
+        // },
+        // { label: 'Google group',
+        // click() { require('electron').shell.openExternal('https://groups.google.com/forum/#!forum/node-red') }
+        // }
+    ]}//, {
+    // label: "Edit",
+    // submenu: [
+    //     { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+    //     { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+    //     { type: "separator" },
+    //     { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+    //     { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+    //     { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+    //     { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+    // ]}, {
+    // label: 'View',
+    // submenu: [
+    //     { label: 'Reload',
+    //         accelerator: 'CmdOrCtrl+R',
+    //         click(item, focusedWindow) { if (focusedWindow) focusedWindow.reload(); }
+    //     },
+    //     { label: 'Toggle Developer Tools',
+    //         accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+    //         click(item, focusedWindow) { if (focusedWindow) focusedWindow.webContents.toggleDevTools(); }
+    //     },
+    //     { type: 'separator' },
+    //     { role: 'resetzoom' },
+    //     { role: 'zoomin' },
+    //     { role: 'zoomout' },
+    //     { type: 'separator' },
+    //     { role: 'togglefullscreen' },
+    //     { role: 'minimize' }
+    // ]}
 ];
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -139,12 +147,12 @@ function createWindow() {
         webPreferences: {
             nodeIntegration: false
         },
-        title: "Node-RED",
+        title: "Pilosol App",
         fullscreenable: true,
         //titleBarStyle: "hidden",
         width: 1024,
         height: 768,
-        icon: __dirname + "/nodered.png"
+        // icon: __dirname + "/nodered.png"
     });
 
     var webContents = mainWindow.webContents;
